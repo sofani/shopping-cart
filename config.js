89c9b9d72e8e8e0b9f7461d2757e11f8bb43b93d
sofani <sofanib@gmail.com>
2017-02-18 14:55:50 -0800

    updated mongoos

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //database: process.env.MONGO_URI || 'mongodb://root:sofa@ds153659.mlab.com:53659/heroku_j8dpvmqm'
-  database: process.env.MONGO_URI || 'localhost/shopping'
+  database: process.env.MONGO_URI || 'mongodb://root:sofa@ds153659.mlab.com:53659/heroku_j8dpvmqm'
+  //database: process.env.MONGO_URI || 'localhost/shopping'
   
 };
